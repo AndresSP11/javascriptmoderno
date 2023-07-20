6894acc8eb134ffaa92ef115668a76311197607d
Andres <andres.salazar.p@uni.pe>
2023-07-20 02:12:32 -0500

    Trabajo añadido

---
+++ javascript/js01.js
@@ -0,0 +1,22 @@
+let precio=200;
+let nombre="Anderson Andres";
+nombre1=true;
+console.log(nombre1);
+nombre=50;
+console.log(nombre);
+const numero1=50;
+
+console.log(typeof(numero1));
+let variable=12;
+console.log(typeof(variable));
+variable="12";
+console.log(typeof(variable));
+console.log("#######PROBANDO NUMERO############");
+const pagina="Welcome to the pagina";
+console.log(typeof pagina);
+console.log("#########CAMBIANDO NUMERO##########");
+
+const texto="20";
+const numerito10=12;
+console.log("La suma es...");
+console.log(numerito10+Number(texto));
